96e6986974f1fa0cedb043f5118348830b199c46
Kestalkayden <kestalkayden@gmail.com>
2020-06-17 10:05:55 -0500

    Show durability for all tools/weapons

--- src/main/java/com/kestalkayden/lonsdaleite/items/tools/Lonsdaleite_Shovel.java
+++ src/main/java/com/kestalkayden/lonsdaleite/items/tools/Lonsdaleite_Shovel.java
@@ -1,7 +1,16 @@
 package com.kestalkayden.lonsdaleite.items.tools;
+import com.kestalkayden.lonsdaleite.materials.KestalToolMaterial;
 
 import net.minecraft.item.ShovelItem;
 import net.minecraft.item.ToolMaterial;
+import net.minecraft.item.ItemStack;
+
+import net.minecraft.client.item.TooltipContext;
+import java.util.List;
+import net.minecraft.text.LiteralText;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
+import net.minecraft.world.World;
 
 public class Lonsdaleite_Shovel extends ShovelItem {
 
@@ -9,4 +18,8 @@ public class Lonsdaleite_Shovel extends ShovelItem {
         super(material, f, attackSpeed, settings);
     }
     
+    @Override
+    public void appendTooltip(ItemStack itemStack, World world, List<Text> tooltip, TooltipContext tooltipContext) {
+        tooltip.add(new LiteralText((KestalToolMaterial.LONSDALEITE.getDurability() - itemStack.getDamage()) +" Durability Left").formatted(Formatting.DARK_PURPLE));
+    }
 }
